95c4160fe5a22952889ac8c56107f353b9270fdb
koishkiss <202300300074@mail.sdu.edu.cn>
2024-09-07 19:56:36 +0800

    init

---
+++ src/main/java/student/forum/StudentForumServerApplication.java
@@ -0,0 +1,13 @@
+package student.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentForumServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentForumServerApplication.class, args);
+	}
+
+}
