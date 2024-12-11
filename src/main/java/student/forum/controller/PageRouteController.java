f9f89a2c6f46b7c2f745cc86e1f8ca2b75b5ad11
koishikiss <202300300074@mail.sdu.edu.cn>
2024-12-11 20:30:09 +0800

    通过转发来允许刷新页面并保持不白屏

--- src/main/java/student/forum/controller/PageRouteController.java
+++ src/main/java/student/forum/controller/PageRouteController.java
@@ -26,4 +26,29 @@ public class PageRouteController {
         return "forward:/index.html";
     }
 
+    @GetMapping("/personal/{child}")
+    public String personalPage1() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping("/personal/{child}/{item}")
+    public String personalPage2() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping("/section/{id}/post/{type}")
+    public String sectionPage() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping("/message/{item}")
+    public String messagePage() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping("/search")
+    public String searchPage() {
+        return "forward:/index.html";
+    }
+
 }
