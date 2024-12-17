15759699b2aaabc043752739ce466d64d633749d
koishikiss <202300300074@mail.sdu.edu.cn>
2024-12-17 12:55:42 +0800

    Update ReplyMapper.java

--- src/main/java/student/forum/mapper/ReplyMapper.java
+++ src/main/java/student/forum/mapper/ReplyMapper.java
@@ -48,16 +48,15 @@ public interface ReplyMapper extends BaseMapper<Reply> {
     @Select("SELECT " +
                 "R.`id` AS `replyId`," +
                 "R.`comment_id` AS `commentId`," +
+                "C.`post_id` AS `postId`," +
                 "R.`uid`," +
                 "R.`reply_time` AS `releaseTime`," +
                 "SUBSTRING(R.`content`,1,50) AS `replyContent`," +
                 "SUBSTRING(C.`content`,1,50) AS `myContent`," +
-                "U.`nickname`," +
-                "P.`id` " +
+                "U.`nickname` " +
             "FROM `reply` R " +
             "JOIN `comment` C ON R.`comment_id`=C.`id` AND C.`uid`=#{uid} " +
             "JOIN `user` U ON U.`uid`=R.`uid` " +
-            "JOIN `post` P ON C.`post_id`=P.`id` " +
             "WHERE R.`call_id`=-1 AND R.`uid`<>#{uid} " +
             "ORDER BY R.`reply_time` DESC " +
             "LIMIT #{pageSize}"
@@ -67,16 +66,15 @@ public interface ReplyMapper extends BaseMapper<Reply> {
     @Select("SELECT " +
                 "R.`id` AS `replyId`," +
                 "R.`comment_id` AS `commentId`," +
+                "C.`post_id` AS `postId`," +
                 "R.`uid`," +
                 "R.`reply_time` AS `releaseTime`," +
                 "SUBSTRING(R.`content`,1,50) AS `replyContent`," +
                 "SUBSTRING(C.`content`,1,50) AS `myContent`," +
-                "U.`nickname`," +
-                "P.`id` " +
+                "U.`nickname` " +
             "FROM `reply` R " +
             "JOIN `comment` C ON R.`comment_id`=C.`id` AND C.`uid`=#{uid} " +
             "JOIN `user` U ON U.`uid`=R.`uid` " +
-            "JOIN `post` P ON C.`post_id`=P.`id` " +
             "WHERE R.`reply_time`<#{startDate} AND R.`call_id`=-1 AND R.`uid`<>#{uid} " +
             "ORDER BY R.`reply_time` DESC " +
             "LIMIT #{pageSize}"
@@ -85,18 +83,18 @@ public interface ReplyMapper extends BaseMapper<Reply> {
 
     @Select("SELECT " +
                 "R1.`id` AS `replyId`," +
+                "R1.`comment_id` AS `commentId`," +
+                "C.`post_id` AS `postId`," +
                 "R1.`call_id` AS `callReplyId`," +
                 "R1.`uid`," +
                 "R1.`reply_time` AS `releaseTime`," +
                 "SUBSTRING(R1.`content`,1,50) AS `replyContent`," +
                 "SUBSTRING(R2.`content`,1,50) AS `myContent`," +
-                "U.`nickname`," +
-                "P.`id` " +
+                "U.`nickname` " +
             "FROM `reply` R1 " +
             "JOIN `reply` R2 ON R1.`id`=R2.`call_id` AND R2.`uid`=#{uid} " +
             "JOIN `user` U ON U.`uid`=R1.`uid` " +
             "JOIN `comment` C ON R1.`comment_id`=C.`id` " +
-            "JOIN `post` P ON C.`post_id`=P.`id` " +
             "WHERE R1.`uid`<>#{uid} " +
             "ORDER BY R1.`reply_time` DESC " +
             "LIMIT #{pageSize}"
@@ -105,18 +103,18 @@ public interface ReplyMapper extends BaseMapper<Reply> {
 
     @Select("SELECT " +
                 "R1.`id` AS `replyId`," +
+                "R1.`comment_id` AS `commentId`," +
+                "C.`post_id` AS `postId`," +
                 "R1.`call_id` AS `callReplyId`," +
                 "R1.`uid`," +
                 "R1.`reply_time` AS `releaseTime`," +
                 "SUBSTRING(R1.`content`,1,50) AS `replyContent`," +
                 "SUBSTRING(R2.`content`,1,50) AS `myContent`," +
-                "U.`nickname`," +
-                "P.`id` " +
+                "U.`nickname` " +
             "FROM `reply` R1 " +
             "JOIN `reply` R2 ON R1.`id`=R2.`call_id` AND R2.`uid`=#{uid} " +
             "JOIN `user` U ON U.`uid`=R1.`uid` " +
             "JOIN `comment` C ON R1.`comment_id`=C.`id` " +
-            "JOIN `post` P ON C.`post_id`=P.`id` " +
             "WHERE R1.`reply_time`<#{startDate} AND R1.`uid`<>#{uid} " +
             "ORDER BY R1.`reply_time` DESC " +
             "LIMIT #{pageSize}"
